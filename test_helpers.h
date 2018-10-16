f1c57e1f9235c3c34f63508fd6c200aaca28fcc5
Adam Pritchard <pritchard.adam@gmail.com>
2018-10-16 16:20:55 -0400

    Add initial working HTTP request test

--- test_helpers.h
+++ test_helpers.h
@@ -5,7 +5,8 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-
+#include <cctype>
+#include <algorithm>
 
 class TempDir
 {
@@ -68,5 +69,15 @@ class TempDir
 
 bool InRange(int64_t target, int64_t low, int64_t high);
 bool IsNear(int64_t target, int64_t comparator, int64_t wiggle);
+
+std::string exec(const char* cmd);
+
+// From https://stackoverflow.com/a/17976541
+inline std::string trim(const std::string &s)
+{
+   auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+   auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+   return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
 
 #endif // PSICASHLIB_TEST_HELPERS_H
