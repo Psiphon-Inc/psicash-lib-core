5a67addd92510095fe3856629708d46db729fb54
Adam Pritchard <pritchard.adam@gmail.com>
2018-10-18 10:00:37 -0400

    Added Datastore write pausing

--- datastore.cpp
+++ datastore.cpp
@@ -14,65 +14,81 @@ using namespace error;
 
 
 Datastore::Datastore()
-    : json_(json::object()) {
+        : json_(json::object()) {
 }
 
-Error Datastore::Init(const char *file_root) {
-  file_path_ = string(file_root) + "/datastore";
+Error Datastore::Init(const char* file_root) {
+    file_path_ = string(file_root) + "/datastore";
 
-  return PassError(FileLoad());
+    return PassError(FileLoad());
 }
 
 void Datastore::Clear() {
-  json_ = json::object();
-  FileStore();
+    json_ = json::object();
+    FileStore();
+}
+
+void Datastore::PauseWrites() {
+  paused_ = true;
+}
+
+error::Error Datastore::UnpauseWrites() {
+    if (!paused_) {
+        return nullerr;
+    }
+    paused_ = false;
+    return FileStore();
 }
 
 Error Datastore::Set(const json& in) {
-  json_.update(in);
-  return PassError(FileStore());
+    json_.update(in);
+    return PassError(FileStore());
 }
 
 Error Datastore::FileLoad() {
-  json_ = json::object();
+    json_ = json::object();
 
-  ifstream f;
-  f.open(file_path_, ios::binary);
+    ifstream f;
+    f.open(file_path_, ios::binary);
 
-  // Figuring out the cause of an open-file problem (i.e., file doesn't exist vs. filesystem is
-  // broken) is annoying difficult to do robustly and in a cross-platform manner.
-  // It seems like these state achieve approximately what we want.
-  // For details see: https://en.cppreference.com/w/cpp/io/ios_base/iostate
-  if (f.fail()) {
-    // File probably doesn't exist. Check that we can write here.
-    return WrapError(FileStore(), "f.fail and FileStore failed");
-  } else if (!f.good()) {
-    return MakeError(utils::Stringer("not f.good; errno=", errno).c_str());
-  }
+    // Figuring out the cause of an open-file problem (i.e., file doesn't exist vs. filesystem is
+    // broken) is annoying difficult to do robustly and in a cross-platform manner.
+    // It seems like these state achieve approximately what we want.
+    // For details see: https://en.cppreference.com/w/cpp/io/ios_base/iostate
+    if (f.fail()) {
+        // File probably doesn't exist. Check that we can write here.
+        return WrapError(FileStore(), "f.fail and FileStore failed");
+    } else if (!f.good()) {
+        return MakeError(utils::Stringer("not f.good; errno=", errno).c_str());
+    }
 
-  try {
-    f >> json_;
-  }
-  catch (json::exception& e) {
-    return MakeError(utils::Stringer("json load failed: ", e.what(), "; id:", e.id).c_str());
-  }
+    try {
+        f >> json_;
+    }
+    catch (json::exception& e) {
+        return MakeError(utils::Stringer("json load failed: ", e.what(), "; id:", e.id).c_str());
+    }
 
-  return nullerr;
+    return nullerr;
 }
 
 Error Datastore::FileStore() {
-  ofstream f;
-  f.open(file_path_, ios::trunc | ios::binary);
-  if (!f.is_open()) {
-    return MakeError(utils::Stringer("not f.is_open; errno=", errno).c_str());
-  }
+    if (paused_) {
+        return nullerr;
+    }
 
-  try {
-    f << json_;
-  }
-  catch (json::exception& e) {
-    return MakeError(utils::Stringer("json dump failed: ", e.what(), "; id:", e.id).c_str());
-  }
+    ofstream f;
+    f.open(file_path_, ios::trunc | ios::binary);
+    if (!f.is_open()) {
+        return MakeError(utils::Stringer("not f.is_open; errno=", errno).c_str());
+    }
 
-  return nullerr;
+    try {
+        f << json_;
+    }
+    catch (json::exception& e) {
+        return MakeError(utils::Stringer("json dump failed: ", e.what(), "; id:", e.id).c_str());
+    }
+
+    return nullerr;
 }
--- datastore.h
+++ datastore.h
@@ -6,72 +6,70 @@
 #include "nonstd/expected.hpp"
 #include "nlohmann/json.hpp"
 
-namespace psicash
-{
+namespace psicash {
 
 // Datastore operations are NOT THREADSAFE.
 //
-class Datastore
-{
-  using json = nlohmann::json;
+class Datastore {
+    using json = nlohmann::json;
 
 public:
-  enum DatastoreGetError {
-    kNotFound = 1,
-    kTypeMismatch
-  };
+    enum DatastoreGetError {
+        kNotFound = 1,
+        kTypeMismatch
+    };
 
 public:
-  Datastore();
+    Datastore();
 
-  // Must be called exactly once.
-  // The fileRoot directory must already exist.
-  // Returns false if there's an unrecoverable error (such as an inability to use the filesystem).
-  error::Error Init(const char *file_root);
+    // Must be called exactly once.
+    // The fileRoot directory must already exist.
+    // Returns false if there's an unrecoverable error (such as an inability to use the filesystem).
+    error::Error Init(const char* file_root);
 
-  // Clears the in-memory structure and the persistent file.
-  // Primarily intended for debugging purposes.
-  void Clear();
+    // Clears the in-memory structure and the persistent file.
+    // Primarily intended for debugging purposes.
+    void Clear();
 
-  // Returns the value, or an error indicating the failure reason.
-  template <typename T>
-  nonstd::expected<T, DatastoreGetError> Get(const char *key) const
-  {
-    try
-    {
-      if (json_.find(key) == json_.end())
-      {
-        return nonstd::make_unexpected(kNotFound);
-      }
+    void PauseWrites();
+    error::Error UnpauseWrites();
 
-      return json_[key].get<T>();
+    // Returns the value, or an error indicating the failure reason.
+    template<typename T>
+    nonstd::expected<T, DatastoreGetError> Get(const char* key) const {
+        try {
+            if (json_.find(key) == json_.end()) {
+                return nonstd::make_unexpected(kNotFound);
+            }
+
+            return json_[key].get<T>();
+        }
+        catch (json::type_error& e) {
+            return nonstd::make_unexpected(kTypeMismatch);
+        }
+        catch (json::out_of_range& e) {
+            // This should be avoided by the explicit check above. But we'll be safe.
+            return nonstd::make_unexpected(kNotFound);
+        }
     }
-    catch (json::type_error &e)
-    {
-      return nonstd::make_unexpected(kTypeMismatch);
-    }
-    catch (json::out_of_range &e)
-    {
-      // This should be avoided by the explicit check above. But we'll be safe.
-      return nonstd::make_unexpected(kNotFound);
-    }
-  }
 
-  // To set a single key-value: `set({{"k1", "v1"}})`.
-  // To set multiple key-values: `set({{"k1", "v1"}, {"k2", "v2"}})`.
-  // NOTE: If you use too few curly braces, you'll accidentally create arrays instead of objects.
-  // NOTE: Set is not atomic. If the file operation fails, the intermediate object will still be
-  // updated. We may want this to be otherwise in the future, but for now I think that it's preferable.
-  // Returns false if the file operation failed.
-  error::Error Set(const json &in);
+    // To set a single key-value: `set({{"k1", "v1"}})`.
+    // To set multiple key-values: `set({{"k1", "v1"}, {"k2", "v2"}})`.
+    // NOTE: If you use too few curly braces, you'll accidentally create arrays instead of objects.
+    // NOTE: Set is not atomic. If the file operation fails, the intermediate object will still be
+    // updated. We may want this to be otherwise in the future, but for now I think that it's preferable.
+    // Returns false if the file operation failed.
+    error::Error Set(const json& in);
 
 protected:
-  error::Error FileLoad();
-  error::Error FileStore();
+    error::Error FileLoad();
+
+    error::Error FileStore();
 
 private:
-  std::string file_path_;
-  json json_;
+    std::string file_path_;
+    json json_;
+    bool paused_;
 };
 
 } // namespace psicash
