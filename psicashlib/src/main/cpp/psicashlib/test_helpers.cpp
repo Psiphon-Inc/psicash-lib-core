a5e192e10063c95a73c445fccb61f6d0948f77ac
Adam Pritchard <pritchard.adam@gmail.com>
2018-10-18 06:43:22 -0400

    Remove helpers that already exists as Gtest asserts

--- psicashlib/src/main/cpp/psicashlib/test_helpers.cpp
+++ psicashlib/src/main/cpp/psicashlib/test_helpers.cpp
@@ -7,14 +7,6 @@
 
 #include "test_helpers.h"
 
-bool InRange(int64_t target, int64_t low, int64_t high) {
-    return (low <= target) && (target <= high);
-}
-
-bool IsNear(int64_t target, int64_t comparator, int64_t wiggle) {
-    return InRange(target, comparator-wiggle, comparator+wiggle);
-}
-
 // From https://stackoverflow.com/a/478960
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
