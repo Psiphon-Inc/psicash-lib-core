a6cde7e75fa67c6222bf893cd9a5d4c4583b8660
Adam Pritchard <pritchard.adam@gmail.com>
2018-10-18 06:42:50 -0400

    Add CullAuthTokens and WritePauser
    Still need to write tests for the latter.

--- psicashlib/src/main/cpp/psicashlib/userdata.h
+++ psicashlib/src/main/cpp/psicashlib/userdata.h
@@ -8,64 +8,74 @@
 #include "error.h"
 #include "nlohmann/json.hpp"
 
-namespace psicash
-{
-extern const char *REQUEST_METADATA; // only for use in template method below
+namespace psicash {
+extern const char* REQUEST_METADATA; // only for use in template method below
 
 using AuthTokens = std::map<std::string, std::string>;
 
 // UserData operations are NOT THREADSAFE.
-class UserData
-{
+class UserData {
 public:
-  UserData();
-  ~UserData();
+    UserData();
 
-  // Must be called once.
-  // Returns false if there's an unrecoverable error (such as an inability to use the filesystem).
-  error::Error Init(const char *file_store_root);
+    ~UserData();
 
-  // Clears data and datastore file.
-  void Clear();
+    // Must be called once.
+    // Returns false if there's an unrecoverable error (such as an inability to use the filesystem).
+    error::Error Init(const char* file_store_root);
+
+    // Clears data and datastore file.
+    void Clear();
+
+    class WritePauser {
+    public:
+        WritePauser(UserData& user_data) : user_data_(
+                user_data) { user_data_.datastore_.PauseWrites(); };
+        ~WritePauser() { (void)Unpause(); }
+        error::Error Unpause() { return user_data_.datastore_.UnpauseWrites(); }
+    private:
+        UserData& user_data_;
+    };
 
 public:
-  datetime::Duration GetServerTimeDiff() const;
-  error::Error SetServerTimeDiff(const datetime::DateTime& serverTimeNow);
+    datetime::Duration GetServerTimeDiff() const;
+    error::Error SetServerTimeDiff(const datetime::DateTime& serverTimeNow);
 
-  AuthTokens GetAuthTokens() const;
-  error::Error SetAuthTokens(const AuthTokens& v, bool is_account);
+    AuthTokens GetAuthTokens() const;
+    error::Error SetAuthTokens(const AuthTokens& v, bool is_account);
+    // valid_token_types is of the form {"tokenvalueABCD0123": true, etc.}
+    error::Error CullAuthTokens(const std::map<std::string, bool>& valid_tokens);
 
-  bool GetIsAccount() const;
-  error::Error SetIsAccount(bool v);
+    bool GetIsAccount() const;
+    error::Error SetIsAccount(bool v);
 
-  int64_t GetBalance() const;
-  error::Error SetBalance(int64_t v);
+    int64_t GetBalance() const;
+    error::Error SetBalance(int64_t v);
 
-  PurchasePrices GetPurchasePrices() const;
-  error::Error SetPurchasePrices(const PurchasePrices& v);
+    PurchasePrices GetPurchasePrices() const;
+    error::Error SetPurchasePrices(const PurchasePrices& v);
 
-  Purchases GetPurchases() const;
-  error::Error SetPurchases(const Purchases& v);
-  error::Error AddPurchase(const Purchase& v);
+    Purchases GetPurchases() const;
+    error::Error SetPurchases(const Purchases& v);
+    error::Error AddPurchase(const Purchase& v);
 
-  TransactionID GetLastTransactionID() const;
-  error::Error SetLastTransactionID(const TransactionID& v);
+    TransactionID GetLastTransactionID() const;
+    error::Error SetLastTransactionID(const TransactionID& v);
 
-  nlohmann::json GetRequestMetadata() const;
-  template <typename T>
-  error::Error SetRequestMetadataItem(const std::string& key, const T& val)
-  {
-    auto j = GetRequestMetadata();
-    j[key] = val;
-    return datastore_.Set({{REQUEST_METADATA, j}});
-  }
+    nlohmann::json GetRequestMetadata() const;
+    template<typename T>
+    error::Error SetRequestMetadataItem(const std::string& key, const T& val) {
+        auto j = GetRequestMetadata();
+        j[key] = val;
+        return datastore_.Set({{REQUEST_METADATA, j}});
+    }
 
 protected:
-  // Modifies the purchases in the argument.
-  void UpdatePurchasesLocalTimeExpiry(Purchases& purchases) const;
+    // Modifies the purchases in the argument.
+    void UpdatePurchasesLocalTimeExpiry(Purchases& purchases) const;
 
 private:
-  Datastore datastore_;
+    Datastore datastore_;
 };
 } // namespace psicash
 
