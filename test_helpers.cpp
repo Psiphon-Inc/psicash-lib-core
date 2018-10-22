55454496dd088d357f9a6ca6754717fdea861a69
Adam Pritchard <pritchard.adam@gmail.com>
2018-10-22 13:41:30 -0400

    Fix double-free crash in debug code

--- test_helpers.cpp
+++ test_helpers.cpp
@@ -11,13 +11,15 @@
 int exec(const char* cmd, std::string& output) {
     output.clear();
     std::array<char, 128> buffer;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    bool closed = false;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), [&closed](FILE* p){if (!closed) pclose(p);});
     if (!pipe) throw std::runtime_error("popen() failed!");
     while (!feof(pipe.get())) {
         if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
             output += buffer.data();
     }
     int result = pclose(pipe.get());
+    closed = true;
     // pclose will be called again as the shared_ptr deleter, but that's okay
     return result;
 }
