5d85e250acbe90e62275b16b7e3edebb9200379f
Adam Pritchard <pritchard.adam@gmail.com>
2018-11-07 14:47:51 -0500

    Improve build

--- build-docker.sh
+++ build-docker.sh
@@ -2,8 +2,8 @@
 set -xeuo pipefail
 
 docker build -t psicashlib .
-docker run --rm -it -v ${PWD}:/app psicashlib ./build.sh
+docker run --rm -v ${PWD}:/app psicashlib ./build.sh
 
 # In theory this is unnecessary, because --rm is supplied to the previous command.
 # In practice --rm doesn't always seem to work as desired.
-docker rm $(docker ps -a  -q -f ancestor=psicashlib); true
+docker rm $(docker ps -a  -q -f ancestor=psicashlib) || :
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+git describe --always --long --dirty --tags > ./psicashlib/src/main/assets/git.txt
+
 ./gradlew :psicashlib:clean
 ./gradlew :psicashlib:assembleRelease
